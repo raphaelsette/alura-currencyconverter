cf25f6793e2ca01a966775b359f829e4dadacc0a
Raphael <raphaelsette@pm.me>
2024-05-21 23:57:17 -0300

    Alteração de layout e adicionado API para consulta de cotação atual

--- script.js
+++ script.js
@@ -1,56 +1,83 @@
-function avisoCotacao(){
-    var avisoCotacao = document.getElementById("avisoCotacao");
-    avisoCotacao.innerHTML = "**Valores referentes a cotação do dia 15/09/2022 03:05PM";
-}
-function converterEmDolar() {
+async function converterEmDolar() {
     var valorEmReais = document.getElementById("valor").value;
     var valorEmReais = parseFloat(valorEmReais);
+    const apiDolar = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+    const res = await fetch(apiDolar)
+    const data = await res.json();
 
-    if(!valorEmReais) {
-        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar o valor em Reais!'});
+    var dataCotacao = new Date().toLocaleString("pt-br");
+    var cotacaoDolar = data.USDBRL.bid
+
+    if (!valorEmReais) {
+        Swal.fire({ icon: 'error', title: 'Oops...', text: 'Você esqueceu de colocar o valor em Reais!' });
     } else {
-        var valorEmDolar = valorEmReais * 5.24;
+        var valorEmDolar = valorEmReais / cotacaoDolar;
         var valorConvertidoEmDolar = document.getElementById("valorConvertido");
         valorConvertidoEmDolar.innerHTML = "R$" + valorEmReais + " reais é igual a $" + valorEmDolar.toFixed(2) + " dolares";
-        avisoCotacao();
+        var avisoCotacao = document.getElementById("avisoCotacao");
+        avisoCotacao.innerHTML = "**Valores referentes a cotação de " + dataCotacao;
     }
 }
-function converterEmEuro() {
+async function converterEmEuro() {
     var valorEmReais = document.getElementById("valor").value;
     var valorEmReais = parseFloat(valorEmReais);
 
-    if(!valorEmReais) {
-        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar o valor em Reais!'});
+    const apiEuro = 'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,GBP-BRL,BTC-BRL'
+    const res = await fetch(apiEuro)
+    const data = await res.json();
+
+    var dataCotacao = new Date().toLocaleString("pt-br");
+    var cotacaoEuro = data.EURBRL.bid
+
+    if (!valorEmReais) {
+        Swal.fire({ icon: 'error', title: 'Oops...', text: 'Você esqueceu de colocar o valor em Reais!' });
     } else {
-        var valorEmEuro = valorEmReais * 5.24;
+        var valorEmEuro = valorEmReais / cotacaoEuro;
         var valorConvertidoEmEuro = document.getElementById("valorConvertido");
         valorConvertidoEmEuro.innerHTML = "R$" + valorEmReais + " reais é igual a €" + valorEmEuro.toFixed(2) + " euros";
-        avisoCotacao();
+        var avisoCotacao = document.getElementById("avisoCotacao");
+        avisoCotacao.innerHTML = "**Valores referentes a cotação de " + dataCotacao;
     }
 }
-function converterEmLibra() {
+async function converterEmLibra() {
     var valorEmReais = document.getElementById("valor").value;
     var valorEmReais = parseFloat(valorEmReais);
 
-    if(!valorEmReais) {
-        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar o valor em Reais!'});
+    const apiLibra = 'https://economia.awesomeapi.com.br/json/last/GBP-BRL'
+    const res = await fetch(apiLibra)
+    const data = await res.json();
+
+    var dataCotacao = new Date().toLocaleString("pt-br");
+    var cotacaoLibra = data.GBPBRL.bid
+
+    if (!valorEmReais) {
+        Swal.fire({ icon: 'error', title: 'Oops...', text: 'Você esqueceu de colocar o valor em Reais!' });
     } else {
-        var valorEmLibra = valorEmReais * 6.01;
+        var valorEmLibra = valorEmReais / cotacaoLibra;
         var valorConvertidoEmLibra = document.getElementById("valorConvertido");
         valorConvertidoEmLibra.innerHTML = "R$" + valorEmReais + " reais é igual a £" + valorEmLibra.toFixed(2) + " libras esterlinas";
-        avisoCotacao();
+        var avisoCotacao = document.getElementById("avisoCotacao");
+        avisoCotacao.innerHTML = "**Valores referentes a cotação de " + dataCotacao;
     }
 }
-function converterEmBitcoin() {
+async function converterEmBitcoin() {
     var valorEmReais = document.getElementById("valor").value;
     var valorEmReais = parseFloat(valorEmReais);
 
-    if(!valorEmReais) {
-        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de colocar o valor em Reais!'});
+    const apiBitcoin = 'https://economia.awesomeapi.com.br/json/last/BTC-BRL'
+    const res = await fetch(apiBitcoin)
+    const data = await res.json();
+
+    var dataCotacao = new Date().toLocaleString("pt-br");
+    var cotacaoBitcoin = data.BTCBRL.bid
+
+    if (!valorEmReais) {
+        Swal.fire({ icon: 'error', title: 'Oops...', text: 'Você esqueceu de colocar o valor em Reais!' });
     } else {
-        var valorEmBitcoin = valorEmReais * 0.0000097;
+        var valorEmBitcoin = valorEmReais / cotacaoBitcoin;
         var valorConvertidoEmBitcoin = document.getElementById("valorConvertido");
         valorConvertidoEmBitcoin.innerHTML = "R$" + valorEmReais + " reais é igual a ₿" + valorEmBitcoin.toFixed(7) + " bitcoins";
-        avisoCotacao();
+        var avisoCotacao = document.getElementById("avisoCotacao");
+        avisoCotacao.innerHTML = "**Valores referentes a cotação de " + dataCotacao;
     }
 }
